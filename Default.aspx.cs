d402ec38aead3295d83e96fe79f520ba99f1cf16
vivek9088 <sridharvivek88@gmail.com>
2015-09-12 22:35:22 -0400

    Modified the sql name in all file for the namespace

---
+++ Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+public partial class _Default : System.Web.UI.Page
+{
+    /*ImageMap[] map;
+    string name;
+    int i = 0;
+    MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["constring"].ConnectionString);
+    MySqlCommand cmd;
+    MySqlDataReader dr;*/
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      /*  con.Open();
+        Panel pan = new Panel();
+        
+        pan.Height = 200;
+        pan.Width = 900;
+        
+        pan.BorderColor = Color.Black;
+        pan.BorderStyle = BorderStyle.Solid;
+        pan.ScrollBars = ScrollBars.Vertical;
+
+       
+       XmlDocument xmlDoc = new XmlDocument();
+       xmlDoc.Load("../addmovies.xml");
+       XmlNodeList dataNodes = xmlDoc.SelectNodes("/Advertisements/Ad");
+       
+        map = new ImageMap[dataNodes.Count];
+        foreach (XmlNode node in dataNodes)
+        {
+          name= node.SelectSingleNode("ImageUrl").InnerText;
+          map[i] = new ImageMap();
+          map[i].ImageUrl = name;
+          map[i].HotSpotMode = HotSpotMode.Navigate;
+          map[i].Height =100;
+          map[i].Width = 200;
+          map[i].Click += new ImageMapEventHandler(imagedetails_Click);
+          pan.Controls.Add(map[i]);
+                  
+          i++;
+        }
+
+        form1.Controls.Add(pan);
+        */
+    }
+
+    protected void imagedetails_Click(object sender, EventArgs e)
+    {
+
+      /*  ImageMap mp = (ImageMap)sender;
+        string s=mp.ImageUrl;
+
+        try
+        {
+            cmd = new MySqlCommand("select * from moviedb where imgpath='" + s + "'", con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            Label1.Text = dr["name"].ToString();
+            Label5.Text = dr["theatre"].ToString();
+            Label3.Text = dr["director"].ToString();
+            Label2.Text = dr["cast"].ToString();
+            Label4.Text = dr["genre"].ToString();
+            dr.Close();
+        }
+        catch (Exception err)
+        {
+            MessageBox.Show("Following Error occured: "+err.Message);
+        }*/
+    }
+}
---
+++ Home.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+public partial class Home : System.Web.UI.Page
+{
+    MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["constring"].ConnectionString);
+    MySqlCommand getuser;
+    MySqlDataReader drg;
+    public string showuser;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.Open();
+        Label1.Text = "Visitor Number : " + Application["usercount"];
+    }
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        string user, pass;
+        showuser = TextBox1.Text;
+        Session["id"] = showuser;
+        if (TextBox1.Text == "")  // if username textbox  is empty
+        {
+            RequiredFieldValidator1.ErrorMessage = "Enter a Valid UserName";
+        }
+
+        MySqlCommand cmd = new MySqlCommand("select * from logindb where username='" + TextBox1.Text + "'", con);
+        MySqlDataReader dr = cmd.ExecuteReader();
+
+
+        if (!dr.Read()) //if user not registered
+        {
+            MessageBox.Show("Please Register YourSelf to be a Valid User", "User Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Response.Redirect("./registeruser.aspx");
+        }
+
+        if (TextBox2.Text == "")  // if password textbox is empty
+        {
+            RequiredFieldValidator2.ErrorMessage = "Enter Password Please";
+        }
+
+        user = dr["username"].ToString();  // retrieve username
+        pass = dr["password"].ToString();  // retrieve password
+
+        if (pass != TextBox2.Text)  // password not equal
+        {
+            MessageBox.Show("Invalid Password!", "Password Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            TextBox2.Text = "";
+            TextBox2.Focus();
+        }
+        if (user.Trim().StartsWith("c") && pass == TextBox2.Text.Trim()) //if user is Customer
+        {
+            getuser = new MySqlCommand("select custname from custdb where custid='" + TextBox1.Text + "'", con);
+            drg = getuser.ExecuteReader();
+            drg.Read();
+            Response.Redirect("./customer/userhomepage.aspx?user=" + drg["custname"].ToString());
+        }
+        else if (user == "emp" && pass == TextBox2.Text.Trim())  //if user is employee
+        {
+            Response.Redirect("./employee/emphomepage.aspx");
+        }
+        else if (user == "admin" && pass == TextBox2.Text.Trim())  //if user is admin
+        {
+            Response.Redirect("./admin/adminhomepage.aspx");
+        }
+
+        dr.Close();
+
+    }
+
+
+}
